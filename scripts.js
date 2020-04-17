e55979eababcb6b652e6c219c6493422a80a4a55
PanagiotisNtymenos <panagiwths.nty@gmail.com>
2020-04-17 22:39:45 +0300

    Pop Ups Done!

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 function logInPopUp(TODO) {
   if (TODO == 'login') {
-    document.querySelector(".popup").style.display = "flex";
+    document.querySelector(".login-popup").style.display = "flex";
   } else if (TODO == 'cancel') {
-    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".login-popup").style.display = "none";
+  }
+}
+
+function signUpPopUp(TODO) {
+  if (TODO == 'signup') {
+    document.querySelector(".signup-popup").style.display = "flex";
+  } else if (TODO == 'cancel') {
+    document.querySelector(".signup-popup").style.display = "none";
   }
 }
